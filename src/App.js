6f0dfeed304777b3f9538cc9608593a52a6bbf22
vivaciouscoder <138902586+vivaciouscoder@users.noreply.github.com>
2024-02-05 11:43:56 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="card">
+          <h1>Weather App</h1>
+          <button className="btn btn-primary">submit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
